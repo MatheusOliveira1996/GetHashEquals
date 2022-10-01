c5afec9e0b6f0cacd1807d14e1ad85a423be8140
Matheus Oliveira <matheus.hortolandia@gmail.com>
2022-10-01 15:57:43 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GetHashEquals.Entities;
+
+namespace GetHashEquals {
+    internal class Program {
+        static void Main (string[] args) {
+
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
